cb41992fe267db019ad2eca5d183474b21dc11f2
lenabenninger <lena.benninger@studentfr.ch>
2025-03-19 21:19:58 +0100

    creation dossier + installation

---
+++ wp_faktenfilter_s6/wp-content/themes/timber-starter-theme/js/index.175edfa9.js
@@ -0,0 +1,12 @@
+/* ----------------------------------
+  imports
+---------------------------------- */ // imports here
+// register plugins here
+/* ----------------------------------
+  main code
+---------------------------------- */ window.addEventListener("DOMContentLoaded", (e)=>{
+// your code here
+});
+
+
+//# sourceMappingURL=index.175edfa9.js.map

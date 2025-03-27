4ed7018ac3e283f14386a422284d4c7891b31ff6
lenabenninger <lena.benninger@studentfr.ch>
2025-03-27 09:12:41 +0100

    corrections

--- wordpress/wp-content/themes/faktenfilter/front-page.php
+++ wordpress/wp-content/themes/faktenfilter/front-page.php
@@ -8,36 +8,50 @@ $context['allgemein_posts'] = Timber::get_posts([
 ]);
 
 // Get 1 post from "empfehlung"
-$context['empfehlung_post'] = Timber::get_posts([
+$empfehlung_posts = Timber::get_posts([
     'posts_per_page' => 1,
     'category_name'  => 'empfehlung',
 ]);
+$context['empfehlung_post'] = !empty($empfehlung_posts) ? $empfehlung_posts[0] : null;
 
 // Fetch "Leserbrief" page by slug
-$page = get_page_by_path('leserbrief'); // Search for the page by its slug
+$page = get_page_by_path('leserbrief');
+error_log('Leserbrief page: ' . print_r($page, true));
 
-if ($page) {
-    // If the page is found, get its details and pass it to the Twig template
-    $context['leserbrief_page'] = Timber::get_post($page->ID);
+if ($page instanceof WP_Post) {
+    $context['leserbrief_page'] = Timber::get_post($page);
 } else {
-    // Debugging: log an error if the page wasn't found
-    error_log('Page "leserbrief" not found!');
+    error_log('Page "Leserbrief" not found!');
 }
 
 // Fetch "Vorschlag" page by slug
-$page = get_page_by_path('vorschlag'); // Search for the page by its slug
+$page = get_page_by_path('vorschlag');
+error_log('Vorschlag page: ' . print_r($page, true));
 
-if ($page) {
-    // If the page is found, get its details and pass it to the Twig template
-    $context['vorschlag_page'] = Timber::get_post($page->ID);
+if ($page instanceof WP_Post) {
+    $context['vorschlag_page'] = Timber::get_post($page);
 } else {
-    // Debugging: log an error if the page wasn't found
-    error_log('Page "vorschlag" not found!');
+    error_log('Page "Vorschlag" not found!');
 }
 
-// Render the front-page.twig template and pass the context to it (only once)
+// Fetch "Hörenswert" page by slug
+$page = get_page_by_path('hoerenswert');
+error_log('Hörenswert page: ' . print_r($page, true));
+
+if ($page instanceof WP_Post) {
+    $context['hoerenswert_page'] = Timber::get_post($page);
+} else {
+    error_log('Page "Hörenswert" not found!');
+}
+
+// Debugging: Check what is inside $context
+error_log(print_r($context, true));
+
+// Render the front-page.twig template and pass the context to it
 Timber::render('front-page.twig', $context);
+
 ?>
+
 
 
 
